ec66cf4c93e5ff7eb9e9d6a7879b1d03c40e8516
YYxxxiiii <axii_@outlook.com>
2020-02-08 23:06:54 +0800

    aa Java

---
+++ java_0208/S2.java
@@ -0,0 +1,15 @@
+package java_0208;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i]!=nums[n]){
+                n+=1;
+                nums[n]=nums[i];
+            }
+        }
+        return n+1;
+    }
+}
+}
