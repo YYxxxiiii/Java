d6ba362abc1c8eac2c4ced5d569d8d8fefa21130
YYxxxiiii <axii_@outlook.com>
2020-02-18 22:03:26 +0800

    aaa Java

---
+++ Java_0218/Class.java
@@ -0,0 +1,33 @@
+package Java_0218;
+
+public class Class {
+    public static void main(String[] args) {//2
+        System.out.println("value="+switchit(4));//3
+    }//4
+    public static int switchit(int x) {
+        int j=1;
+        switch (x) {
+            case 1:j++;
+            case 2:j++;
+            case 3:j++;
+            case 4:j++;
+            case 5:j++;
+            default:j++;
+        } return j+x;
+    }
+
+
+
+
+public int lengthOfLastWord(String s) {
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            } else if (length != 0) {
+                return length;
+            }
+        }
+        return length;
+    }
+}
