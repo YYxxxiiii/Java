fa846477c09b30b52d3d68232feddb2f01ea3a62
YYxxxiiii <axii_@outlook.com>
2020-03-01 19:19:14 +0800

    aa Java

---
+++ Java_0301/LinkedList.java
@@ -0,0 +1,11 @@
+package Java_0301;
+
+public class LinkedList {
+
+    if (head == null) {
+        return null;
+    }
+    ListNode prev = head;
+    ListNOde cur = head.next;
+    while (cur !=null)
+}
