e69087018d99e910fe527d9c5fe62593fbff50ad
YYxxxiiii <axii_@outlook.com>
2020-05-09 23:12:57 +0800

    aa

---
+++ MyStack.java
@@ -0,0 +1,20 @@
+public class MyStack {
+    private int[] array = new int[100];
+    private int size = 0;
+
+    //入栈
+    public void push(int x) {
+        array[size] = x;
+        size++;
+    }
+    //取栈顶元素
+    public int pop(int x){
+        return array[size - 1];
+    }
+    //出栈
+    public int peek(){
+        int ret = array[size - 1];
+        size--;
+        return ret;
+    }
+}
