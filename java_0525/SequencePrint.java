8482ac4cfd345cfa1d2d767c9b2c3612b9f61338
YYxxxiiii <axii_@outlook.com>
2020-05-26 18:40:33 +0800

    同时打印ABC

--- java_0525/SequencePrint.java
+++ java_0525/SequencePrint.java
@@ -17,28 +17,39 @@ public class SequencePrint {
     //每个线程循环10次来打印A.B.C
     //每次打印之后,等待其余线程打印完
     public static void print1() {
-        Thread t1 = new Thread(new Print("A"));
-        Thread t2 = new Thread(new Print("B"));
-        Thread t3 = new Thread(new Print("C"));
+        Thread t1 = new Thread(new Print("A", null));
+        Thread t2 = new Thread(new Print("B", t1));
+        Thread t3 = new Thread(new Print("C", t2));
+        t1.start();
+        t2.start();
+        t3.start();
     }
 
     public static void main(String[] args) {
-
+        print1();
     }
 
     //对该类进行包装,实现打印(传进来是啥,就打印啥)
     private  static class Print implements Runnable{
         private String content;
-
+        private Thread t;
 
         //可以走外部传进来
-        public Print(String content) {
+        public Print(String content,Thread t) {
             this.content = content;
+            this.t = t;
         }
 
         @Override
         public void run() {
-            System.out.println(content);
+            try {
+                if (t != null) {
+                    t.join();
+                }
+                System.out.println(content); //content就是打印你传进来的内容
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
