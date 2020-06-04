14fb769accc445447a52b3261ca929d059c584c2
YYxxxiiii <axii_@outlook.com>
2020-06-04 15:41:50 +0800

    汽水瓶

---
+++ java_0604/Main.java
@@ -0,0 +1,27 @@
+package java_0604;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static int change(int n) {
+        int count = 0;
+        while(n > 2) {
+            count += n/3;
+            n = n/3 + n%3;
+        }
+        if(n == 2) {
+            count += 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        while (sc.hasNext()) {
+            n = sc.nextInt();
+            System.out.println(change(n));
+        }
+
+    }
+}
