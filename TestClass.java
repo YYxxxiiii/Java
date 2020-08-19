ed2b62b10e108b00468e14ae55cb061945470463
YYxxxiiii <axii_@outlook.com>
2020-08-19 22:31:32 +0800

    a

---
+++ TestClass.java
@@ -0,0 +1,36 @@
+package java_0806;
+
+public class TestClass {
+
+//    private static void testMethod(){
+//
+//        System.out.println("testMethod");
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        ((TestClass)null).testMethod();
+//
+//    }
+//    public void getCustomer() {
+//        try {
+//
+//            // do something that may cause an Exception
+//
+//        } catch (java.io.FileNotFoundException ex) {
+//
+//            System.out.print("FileNotFoundException!");
+//
+//        } catch (java.io.IOException ex) {
+//
+//            System.out.print("IOException!");
+//
+//        } catch (java.lang.Exception ex) {
+//
+//            System.out.print("Exception!");
+//
+//        }
+//    }
+
+}
